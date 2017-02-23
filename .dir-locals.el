601c3689908164de8bb7138f8c23ce0f4c2f5afe
Yann Herklotz <ymherklotz@gmail.com>
2017-02-23 12:40:24 +0000

    Parser working completely now

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,3 @@
 ((nil . ((company-clang-arguments . ("-I../include/")) 
-	 (company-c-headers-path-user . ("../include/")))))
+	 (company-c-headers-path-user . ("../include/"))
+	 (flycheck-clang-include-path . ("../include")))))
