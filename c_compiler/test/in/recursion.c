2ba77a78a37448eeeff59ed10fff06676b911fb7
Yann Herklotz <ymherklotz@gmail.com>
2017-03-19 23:33:08 +0000

    Making recursion nicer

--- c_compiler/test/in/recursion.c
+++ c_compiler/test/in/recursion.c
@@ -1,12 +1,11 @@
 int fact(int n)
 {
-    if(n <= 1) {
+    if(n <= 1)
 	return 1;
-    } else {
+    else
 	return fact(n-1) * n;
-    }
 }
 
 int main() {
-    return fact(5);
+    return fact(5) + fact(2) + fact(1);
 }
